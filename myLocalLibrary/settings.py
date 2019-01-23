be5d2f4c4278ce5aba34026df699d6ca6341afa8
unknown <marcusviniciusdasilveiraricha@gmail.com>
2019-01-22 23:54:48 -0200

    config static

--- myLocalLibrary/settings.py
+++ myLocalLibrary/settings.py
@@ -120,8 +120,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 # Redirection to home URL after login (Attention: Django default will redirect to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
